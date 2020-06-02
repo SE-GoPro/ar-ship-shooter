32105e11daa61c3ed0884b8c70da778412d13ff7
khitokuroba <anhtd98@gmail.com>
2020-06-03 05:41:54 +0700

    sound and animation

--- Assets/Scripts/OptionMenu.cs
+++ Assets/Scripts/OptionMenu.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class OptionMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+
+    public AudioMixer audioMixer;
+    public void SetVolumn(float volume)
     {
-        
+        audioMixer.SetFloat("volume", volume);
     }
 
     // Update is called once per frame
