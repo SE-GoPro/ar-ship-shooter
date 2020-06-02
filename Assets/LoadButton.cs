c7b9d9d23dc82a1cf66e3d71cb4415ec166458b7
khitokuroba <anhtd98@gmail.com>
2020-06-02 22:21:42 +0700

    heart bar

---
+++ Assets/LoadButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadButton : MonoBehaviour
+{
+    [SerializeField]
+    private InputField inputField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(TryLoadScene);
+    }
+
+    public void TryLoadScene()
+    {
+        string sceneName = inputField.text;
+
+        if (sceneName != null)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+        else
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
